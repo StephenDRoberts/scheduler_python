0ae9e7da8cfdb44e9e0e08d1e40b4a5198e14fc4
Stephen Roberts <stephen.roberts2@skybettingandgaming.com>
2022-11-11 00:33:08 +0000

    [004] trying to mock test

--- src/parser/parse_csv.py
+++ src/parser/parse_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-# TODO defensive coding around bad csv formats
 
+# TODO defensive coding around bad csv formats
 def parse(filepath):
     data = pd.read_csv(filepath)
     df = pd.DataFrame(data)
