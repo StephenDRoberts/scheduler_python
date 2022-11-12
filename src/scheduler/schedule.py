200dc807246841507fe702bb2237f3e1aafcc7de
Stephen Roberts <stephen.roberts2@skybettingandgaming.com>
2022-11-12 10:02:23 +0000

    [006] - move to one process

--- src/scheduler/schedule.py
+++ src/scheduler/schedule.py
@@ -49,6 +49,12 @@ def schedule(tasks, collectors, preferences):
         tasks_excl_partially_processed = tasks_excl_fully_processed[
             ~tasks_excl_fully_processed.loc[:, 'task_id'].isin(partially_processed_tasks['task_id'])]
 
+        tasks_excl_partially_processed = tasks_excl_partially_processed.sort_values(
+            by=['processing_deadline', 'percentage_complete'],
+            ascending=[True, False]
+        )
+
+
         # filter for employees working in this shift
         employees_on_shift = sorted_collectors_by_shift[
             sorted_collectors_by_shift.loc[:, 'shift_end_datetime'] == shift_end]
@@ -62,28 +68,15 @@ def schedule(tasks, collectors, preferences):
         capacity_used = 0
         # process partially processed matches as priority
         # once complete, place these games in processed_tasks and remove from partially_processed_tasks
-        for index in range(len(partials_for_todays_shift)):
+        for index in range(len(tasks_excl_fully_processed)):
             capacity_used += 1
-            partial_task = partials_for_todays_shift.iloc[index]
+            task = tasks_excl_fully_processed.iloc[index]
 
             # TODO need to make this a universal capacity check
             # if overcapacity for number of employee tasks - exit
             if capacity_used >= (len(employees_on_shift.index) * MAX_TASKS_PER_EMPLOYEE):
                 continue
 
-            state_after_process = process_task(
-                partial_task, employees_on_shift, preferences,
-                processed_tasks, partially_processed_tasks
-            )
-
-            processed_tasks = state_after_process['processed_tasks']
-            partially_processed_tasks = state_after_process['partially_processed_tasks']
-
-        # process new tasks
-        for task_index, task in tasks_excl_partially_processed.iterrows():
-            capacity_used += 1
-            if capacity_used >= (len(employees_on_shift.index) * MAX_TASKS_PER_EMPLOYEE):
-                continue
             state_after_process = process_task(
                 task, employees_on_shift, preferences,
                 processed_tasks, partially_processed_tasks
